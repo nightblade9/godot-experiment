8f4efb6e93b45e61fe97f151bcf35684063ff9dc
nightblade9 <nightbladecodes@gmail.com>
2018-06-21 00:18:18 -0400

    Convert movement into component

--- entities/Player.gd
+++ entities/Player.gd
@@ -1,32 +1,10 @@
 # Use KinematicBody2D for keyboard movement
 extends RigidBody2D
 
-export (int) var acceleration = 2000
-export (int) var max_movement_speed = 800
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
 func _integrate_forces(state):
-	var velocity = state.get_linear_velocity()
-	
-	var gained_velocity = acceleration * state.step
-	
-	if Input.is_key_pressed(KEY_LEFT) and velocity.x > -max_movement_speed:
-		velocity.x -= gained_velocity
-	if Input.is_key_pressed(KEY_RIGHT) and velocity.x < max_movement_speed:
-		velocity.x += gained_velocity
-	if Input.is_key_pressed(KEY_UP) and velocity.y > -max_movement_speed:
-		velocity.y -= gained_velocity
-	if Input.is_key_pressed(KEY_DOWN) and velocity.y < max_movement_speed:
-		velocity.y += gained_velocity
-	
-	velocity = Vector2(lerp(velocity.x, 0, 0.05), lerp(velocity.y, 0, 0.05))
-	velocity += state.get_total_gravity() * state.step
-	state.set_linear_velocity(velocity)
+	$PhysicsKeyboardMove2D._integrate_forces(state)
